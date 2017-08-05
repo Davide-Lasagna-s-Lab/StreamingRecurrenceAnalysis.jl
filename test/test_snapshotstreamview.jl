f601d5999df6e635e63bf0f520831c96826a9a04
Davide Lasagna <davide.lasagna@soton.ac.uk>
2017-08-05 16:00:32 +0100

    added tests for snapshotstreamview

--- test/test_snapshotstreamview.jl
+++ test/test_snapshotstreamview.jl
@@ -1,7 +1,7 @@
 using Base.Test
 using StreamingRecurrenceAnalysis
 
-@testset "iteration protocol                     " begin
+@testset "iteration protocol - width != 1        " begin
     g(x₀) = (x₀ .+= 1; x₀)
     output = ([[3], [2], [1]],
               [[4], [3], [2]],
@@ -30,14 +30,61 @@ using StreamingRecurrenceAnalysis
     end
 end
 
+@testset "iteration protocol - window width = 1  " begin
+    g(x₀) = (x₀ .+= 1; x₀)
+    output = ([[0]], [[1]], [[2]], [[3]], [[4]], [[5]], [[6]], [[7]])
+    for N in 0:7
+        sview = snapshot_stream_view(g, [0], 1, N)
+        for (i, v) in enumerate(sview)
+            @test v == output[i]
+            @test i <= N+1 # N+1 views are generated
+        end
+    end
+end
+
 @testset "error checking                         " begin
-    @test_throws ArgumentError snapshot_stream_view((), [1], 5, 1)
-    @test_throws ArgumentError snapshot_stream_view((), [1], 5, 2)
-    @test_throws ArgumentError snapshot_stream_view((), [1], 5, 3)
+    @test_throws ArgumentError snapshot_stream_view((), [1],  5, 1)
+    @test_throws ArgumentError snapshot_stream_view((), [1],  5, 2)
+    @test_throws ArgumentError snapshot_stream_view((), [1],  5, 3)
+    @test_throws ArgumentError snapshot_stream_view((), [1],  0, 0)
+    @test_throws ArgumentError snapshot_stream_view((), [1],  0, 1)
+    @test_throws ArgumentError snapshot_stream_view((), [1], -1, 1)
 end
 
 @testset "length                                 " begin
-    @test length(snapshot_stream_view((), [1], 3, 2)) == 1
-    @test length(snapshot_stream_view((), [1], 3, 3)) == 2
-    @test length(snapshot_stream_view((), [1], 3, 4)) == 3
+    g(x₀) = (x₀ .+= 1; x₀)
+    for width = 1:5
+        for N = (width-1):10
+            sview = snapshot_stream_view(g, [0], width, N)
+            @test length(collect(sview)) == length(sview)
+        end
+    end
+end
+
+@testset "example usage with allocation          " begin
+    
+    # initial condition and temporary
+    x  = randn(100)
+    u  = similar(x) 
+
+    # increment state
+    g(x₀) = (x₀ .+= 1.0; x₀)
+
+    # have a stream of 10 snapshots with a view of width 2
+    sview = snapshot_stream_view(g, x, 20, 100)
+
+    # sum
+    function dowork!(sview::SnapshotStreamView{X}, u::X) where {X}
+        for v in sview     # the current view
+            for snap in v  # each snapshot in the view 
+                u .+= snap # do stuff
+            end
+        end
+        u    
+    end
+
+    # warm up
+    dowork!(sview, u)
+        
+    @test (@allocated dowork!(sview, u)) == 0
 end
