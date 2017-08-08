bc5d70c9beca12290d48b1a22e27e437d73473c8
Davide Lasagna <davide.lasagna@soton.ac.uk>
2017-08-08 17:32:29 +0200

    implemented basic structure

---
+++ src/recurrence.jl
@@ -0,0 +1,110 @@
+# ~~~ TILE ~~~
+struct Tile{T} <: AbstractMatrix{T}
+    data::NTuple{3, NTuple{3, T}}
+end
+Base.size(t::Tile) = (3, 3)
+Base.getindex(t::Tile, i, j) = t.data[i][j] 
+
+centre(t::Tile) = t[2, 2] 
+
+@inline function isminimum(t::Tile) 
+    (a, b, c), (d, e, f), (g, h, i) = t.data
+    e < min(min(a, b, c, d), min(f, g, h, i))
+end
+
+# ~~~ Tile iterator over a slice ~~~
+struct TileIterator{D}
+              R::Vector{Vector{D}}
+    startoffset::Int
+end
+
+@inline Base.start(s::TileIterator)        = s.startoffset
+@inline Base.done( s::TileIterator, state) = state == length(s.R[1])-1
+@inline function Base.next(s::TileIterator, state)
+    @inbounds begin
+        a = s.R[1][state+2]; b = s.R[2][state+2]; c = s.R[3][state+2]
+        d = s.R[1][state+1]; e = s.R[2][state+1]; f = s.R[3][state+1]
+        g = s.R[1][state  ]; h = s.R[2][state  ]; i = s.R[3][state  ]
+    end
+    # return the offset of the current tile and the tile
+    (state, Tile(((a, b, c),
+                  (d, e, f),
+                  (g, h, i)))), state+1
+end
+
+# ~~~ THE STREAMING RECURRENCE MATRIX ~~~
+struct StreamDistMatrix{S<:StreamView, F, D}
+          sview::S
+           dist::F
+    startoffset::Int
+              R::Vector{Vector{D}} # three vectors of eltype D
+end
+
+function streamdistmat(sview::StreamView, dist, startoffset::Int)
+    # get one element of the stream, compute distance and 
+    # initialise vectors R with appropriate type
+    x = sview.buffer[1]
+    res = dist(x, x)
+    R = Vector{typeof(res)}[[zero(res) for i = 1:width(sview)] for j = 1:3]
+    StreamDistMatrix(sview, dist, startoffset, R)
+end
+
+# ~~~ Iteration over the slices ~~~
+@inline function Base.start(srm::StreamDistMatrix)
+    _, state = _advance(srm, start(srm.sview))
+    _, state = _advance(srm, state)
+    state
+end
+
+@inline Base.next(srm::StreamDistMatrix, state) = _advance(srm, state)
+
+@inline function _advance(srm::StreamDistMatrix, state)
+    # get new snapshots
+    window, state = next(srm.sview, state)
+    # compute distances and shift
+    insert!(srm.R, 
+            length(srm.R), 
+            _kernel(shift!(srm.R), srm.startoffset, window, srm.dist))
+    srm.R, state
+end
+
+@inline function _kernel(r, startoffset, window, dist)
+    for i in startoffset:length(r)
+        r[i] = dist(window[1], window[i])
+    end
+    r
+end
+
+@inline Base.done(srm::StreamDistMatrix, state) = done(srm.sview, state)
+
+# ~~~ Iteration over all tiles ~~~
+struct Tiles{D<:StreamDistMatrix}
+    d::D
+end
+
+@inline Base.start(t::Tiles) = start(t.d)
+@inline function Base.next(t::Tiles, state) 
+    R, state = next(t.d, state)
+    TileIterator(R, t.d.startoffset), state
+end
+@inline Base.done(t::Tiles, state) = done(t.d, state)
+
+tiles(d::StreamDistMatrix) = Iterators.flatten(Tiles(d))
+
+# ~~~ Iterator over Recurrences  ~~~
+recurrences(R::StreamDistMatrix) = 
+    Iterators.filter((data)->isminimum(data[2]), tiles(R))
+
+# ~~~ Iteration over the entries ~~~
+struct Entries{T<:Tiles}
+    tiles::T
+end
+
+entries(R::StreamDistMatrix) = Entries(tiles(R))
+
+@inline Base.start(s::Entries) = start(s.tiles)
+@inline function Base.next(s::Entries, state) 
+    tile, state = next(s.tiles, state)
+    (i, j, value(centre(tile))), state
+end
+@inline Base.done(s::Entries) = done(s.tiles, state)
---
+++ test/test_recurrence.jl
@@ -0,0 +1,273 @@
+using BenchmarkTools
+using Base.Test
+import StreamingRecurrenceAnalysis: Tile, 
+                                    isminimum, 
+                                    TileIterator, 
+                                    streamview,
+                                    streamdistmat,
+                                    tiles,
+                                    recurrences,
+                                    centre
+
+@testset "Tile                                   " begin
+    t = Tile(((1, 2, 3),
+              (1, 0, 3),
+              (1, 2, 3)))
+    @test isminimum(t) == true
+
+    t = Tile(((1, 0, 3),
+              (1, 4, 3),
+              (1, 2, 3)))
+    @test isminimum(t) == false
+
+    t = Tile(((1, 1, 1),
+              (1, 1, 1),
+              (1, 1, 1)))
+    @test isminimum(t) == false
+
+    t = Tile(((1, 2, 1),
+              (1, 1, 1),
+              (1, 1, 1)))
+    @test isminimum(t) == false
+end
+
+@testset "TileIterator                           " begin
+    dists = [[0, 2, 4, 0, 4, 1], 
+             [0, 2, 2, 1, 8, 6],
+             [0, 6, 1, 8, 3, 9]]
+
+    # moving slice
+    sl = TileIterator(dists, 1)
+
+    # expected output
+    output = [(1, Tile(((4, 2, 1),   # tile centred at index two
+                        (2, 2, 6),   # for an offset equal to one
+                        (0, 0, 0)))),
+              (2, Tile(((0, 1, 8), 
+                        (4, 2, 1), 
+                        (2, 2, 6)))),
+              (3, Tile(((4, 8, 3),
+                        (0, 1, 8), 
+                        (4, 2, 1)))),
+              (4, Tile(((1, 6, 9),
+                        (4, 8, 3),
+                        (0, 1, 8))))]
+    # typical usage             
+    for (i, (offset, tile)) in enumerate(sl)
+        @test output[i] == (offset, tile)
+    end
+end
+
+@testset "streamdistmat                          " begin
+    # generate a known stream of Ints
+    counter = 1
+    data = [1, 3, 5, 1, 4, 7, 9, 2, 8, 3, 1, 0]
+    g(x₀) = data[counter+=1]
+    dist(x, y) = abs(x-y)
+
+    sview = streamview(g, data[1], 4, 10)
+
+    # these will be the views generated
+    views = Vector{Int}[[1, 3, 5, 1],
+                        [3, 5, 1, 4],
+                        [5, 1, 4, 7],
+                        [1, 4, 7, 9],
+                        [4, 7, 9, 2],
+                        [7, 9, 2, 8],
+                        [9, 2, 8, 3],
+                        [2, 8, 3, 1]]
+    # test they actually are                        
+    @test [copy(view) for view in sview] == views             
+             
+    # streaming distance matrix
+    R = streamdistmat(sview, dist, 1)
+
+    # this will be the distances calculated             
+    dists = [[0, 2, 4, 0], 
+             [0, 2, 2, 1],
+             [0, 4, 1, 2],
+             [0, 3, 6, 8],
+             [0, 3, 5, 2],
+             [0, 2, 5, 1],
+             [0, 7, 1, 6],
+             [0, 6, 1, 1]]
+
+    # these will be the triplets generated    
+    output = [[[0, 2, 4, 0], 
+               [0, 2, 2, 1],
+               [0, 4, 1, 2]],
+              [[0, 2, 2, 1],
+               [0, 4, 1, 2],
+               [0, 3, 6, 8]],
+              [[0, 4, 1, 2],
+               [0, 3, 6, 8],
+               [0, 3, 5, 2]],
+              [[0, 3, 6, 8],
+               [0, 3, 5, 2],
+               [0, 2, 5, 1]],
+              [[0, 3, 5, 2],
+               [0, 2, 5, 1],
+               [0, 7, 1, 6]],
+              [[0, 2, 5, 1],
+               [0, 7, 1, 6],
+               [0, 6, 1, 1]]]  
+
+    # reset counter for data generation
+    counter = 1     
+    sview = streamview(g, data[1], 4, 10)
+    for (i, r) in enumerate(R)
+        @test output[i] == r
+    end
+end
+
+@testset "tiles                                  " begin
+    # generate a known stream of Ints
+    counter = 1
+    data = [1, 3, 5, 1, 4, 7, 9, 2, 8, 3, 1, 0]
+    g(x₀) = data[counter+=1]
+    dist(x, y) = abs(x-y)
+    sview = streamview(g, data[1], 4, 10)
+                    
+    # streaming distance matrix
+    R = streamdistmat(sview, dist, 1)
+
+    # these will be the triplets generated    
+    # output = [[[0, 2, 4, 0], 
+    #            [0, 2, 2, 1],
+    #            [0, 4, 1, 2]],
+    #           [[0, 2, 2, 1],
+    #            [0, 4, 1, 2],
+    #            [0, 3, 6, 8]],
+    #           [[0, 4, 1, 2],
+    #            [0, 3, 6, 8],
+    #            [0, 3, 5, 2]],
+    #           [[0, 3, 6, 8],
+    #            [0, 3, 5, 2],
+    #            [0, 2, 5, 1]],
+    #           [[0, 3, 5, 2],
+    #            [0, 2, 5, 1],
+    #            [0, 7, 1, 6]],
+    #           [[0, 2, 5, 1],
+    #            [0, 7, 1, 6],
+    #            [0, 6, 1, 1]]]  
+    # use this for generation
+    # for o in output
+    #     D = hcat(o...)
+    #     display(flipdim(D[1:3, :], 1)); println()
+    #     display(flipdim(D[2:4, :], 1)); println()
+    # end
+
+    ts = [(1, Tile(((4,  2,  1),
+                    (2,  2,  4),
+                    (0,  0,  0)))),
+          (2, Tile(((0,  1,  2),
+                    (4,  2,  1),
+                    (2,  2,  4)))),
+          (1, Tile(((2,  1,  6),
+                    (2,  4,  3),
+                    (0,  0,  0)))),
+          (2, Tile(((1,  2,  8),
+                    (2,  1,  6),
+                    (2,  4,  3)))),
+          (1, Tile(((1,  6,  5),
+                    (4,  3,  3),
+                    (0,  0,  0)))),
+          (2, Tile(((2,  8,  2),
+                    (1,  6,  5),
+                    (4,  3,  3)))),
+          (1, Tile(((6,  5,  5),
+                    (3,  3,  2),
+                    (0,  0,  0)))),
+          (2, Tile(((8,  2,  1),
+                    (6,  5,  5),
+                    (3,  3,  2)))),
+          (1, Tile(((5,  5,  1),
+                    (3,  2,  7),
+                    (0,  0,  0)))),
+          (2, Tile(((2,  1,  6),
+                    (5,  5,  1),
+                    (3,  2,  7)))),
+          (1, Tile(((5,  1,  1),
+                    (2,  7,  6),
+                    (0,  0,  0)))),
+          (2, Tile(((1,  6,  1),
+                    (5,  1,  1),
+                    (2,  7,  6))))]
+
+    for (i, (offset, tile)) in enumerate(tiles(R))
+       @test ts[i] == (offset, tile)
+    end
+end
+
+@testset "recurrences                            " begin
+   # generate a known stream of Ints
+    counter = 1
+    data = [1, 2, 3, 1, 3, 4, 2, 6]
+    g(x₀) = data[counter+=1]
+    dist(x, y) = abs(x-y)
+
+    # test views
+    sview = streamview(g, data[1], 5, length(data)-1)
+
+    output = [[1, 2, 3, 1, 3],
+              [2, 3, 1, 3, 4],
+              [3, 1, 3, 4, 2],
+              [1, 3, 4, 2, 6]]
+
+    for (i, s) in enumerate(sview)
+        @test output[i] == s
+    end
+
+    # test streaming distance matrix
+    counter = 1
+    sview = streamview(g, data[1], 5, length(data)-1)
+    R = streamdistmat(sview, dist, 1)
+
+    output = [[[0, 1, 2, 0, 2], 
+               [0, 1, 1, 1, 2], 
+               [0, 2, 0, 1, 1]],
+              [[0, 1, 1, 1, 2], 
+               [0, 2, 0, 1, 1], 
+               [0, 2, 3, 1, 5]]]
+
+    for (i, s) in enumerate(R)
+        @test output[i] == s
+    end
+
+    # test tiles
+    output = [(1, Tile(((2,  1,  0),
+                        (1,  1,  2),
+                        (0,  0,  0)))),
+              (2, Tile(((0,  1,  1),
+                        (2,  1,  0),
+                        (1,  1,  2)))),
+              (3, Tile(((2,  2,  1),
+                        (0,  1,  1),
+                        (2,  1,  0)))),
+              (1, Tile(((1,  0,  3),
+                        (1,  2,  2),
+                        (0,  0,  0)))),
+              (2, Tile(((1,  1,  1),
+                        (1,  0,  3),
+                        (1,  2,  2)))),
+              (3, Tile(((2,  1,  5),
+                        (1,  1,  1),
+                        (1,  0,  3))))]
+    counter = 1
+    sview = streamview(g, data[1], 5, length(data)-1)
+    R = streamdistmat(sview, dist, 1)
+    for (i, (offset, tile)) in enumerate(tiles(R))
+        @test output[i] == (offset, tile)
+    end
+
+    # test recurrences
+    output = [(2, Tile(((1,  1,  1),
+                        (1,  0,  3),
+                        (1,  2,  2)))),]
+    counter = 1
+    sview = streamview(g, data[1], 5, length(data)-1)
+    R = streamdistmat(sview, dist, 1)
+    for (i, (offset, tile)) in enumerate(recurrences(R))
+        @test output[i] == (x, offset)
+    end
+end
